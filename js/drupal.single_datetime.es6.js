b9a055af3bd63bbb6b70ef88c8bfd7f03b2cdb87
valic <valic@980550.no-reply.drupal.org>
2018-11-25 12:40:01 +0100

    Issue #2994273 by VitaliyB98, valic: Error in IE 11 with date picker

---
+++ js/drupal.single_datetime.es6.js
@@ -0,0 +1,134 @@
+/**
+ * @file
+ * Javascript to initialize singe date time library.
+ */
+
+(($, Drupal, drupalSettings) => {
+  /**
+   * Function to format date.
+   *
+   * @param {string} $type
+   *   Granularity for allowed intervals in minutes.
+   * @return {array}
+   *   Return minutes formatted in array by allowed intervals.
+   */
+  function SingleDatetimeAllowTimes($type) {
+    const times = [];
+
+    // Build array.
+    for (let i = 0; i < 24; i++) {
+      // Formatting hours.
+      if (i < 10) {
+        i = `0${i}`;
+      }
+
+      // Default granularity is one hour.
+      if ($type === 60) {
+        times.push(`${i}:00`);
+      }
+
+      // Custom settings, per minutes.
+      else {
+        const measure = 60 / $type;
+        for (let j = 0; j < measure; j++) {
+          let minutes = $type * j;
+
+          if (j === 0) {
+            minutes = "00";
+          } else if (minutes < 10) {
+            minutes = `0${minutes}`;
+          }
+          times.push(`${i}:${minutes}`);
+        }
+      }
+    }
+
+    return times;
+  }
+
+  /**
+   * Attaches the single_datetime behavior.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.single_datetime = {
+    attach(context) {
+      // Setting the current language for the calendar.
+      const lang = drupalSettings.path.currentLanguage;
+
+      $(context)
+        .find("input[data-single-date-time]")
+        .once("datePicker")
+        .each(() => {
+          const input = $(this);
+
+          // Get widget type.
+          const widgetType = input.data("singleDateTime");
+
+          // Get hour format - 12 or 24.
+          const hourFormat = input.data("hourFormat");
+
+          // Get first day in week from Drupal.
+          const dayOfWeekStart = input.data("firstDay");
+
+          // Default values (used for dates only).
+          let format = "Y-m-d";
+          let allowTimepicker = false;
+
+          // Get minute granularity
+          const allowTimes = SingleDatetimeAllowTimes(input.data("allowTimes"));
+
+          // Get disabled days.
+          const disabledWeekDays = input.data("disableDays");
+
+          // Get excluded dates.
+          const disabledDates = input.data("excludeDate");
+
+          // Get minimum date.
+          const minDate = input.data("minDate");
+
+          // Get maximum date.
+          const maxDate = input.data("maxDate");
+
+          // Get start year.
+          const yearStart = input.data("yearStart");
+
+          // Get end year.
+          const yearEnd = input.data("yearEnd");
+
+          // Set the hour format.
+          const formatTime = hourFormat === "12h" ? "h:i A" : "H:i";
+
+          const inline = input.data("inline");
+
+          const theme = input.data("datetimepickerTheme");
+
+          // If is date & time field.
+          if (widgetType === "datetime") {
+            format = hourFormat === "12h" ? "Y-m-d h:i:s A" : "Y-m-d H:i:s";
+            allowTimepicker = true;
+          }
+
+          $(`#${input.attr("id")}`).datetimepicker({
+            lang,
+            format,
+            formatTime,
+            lazyInit: true,
+            timepicker: allowTimepicker,
+            todayButton: true,
+            dayOfWeekStart,
+            allowTimes,
+            disabledWeekDays,
+            disabledDates,
+            formatDate: "d.m.Y",
+            inline,
+            minDate,
+            maxDate,
+            yearStart,
+            yearEnd,
+            theme
+          });
+        });
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
